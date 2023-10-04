63feb8f3cbc91ef8d7496186a0b652bcff8f873e
ravi2krishna <ravi2krishna@gmail.com>
2023-10-04 14:12:51 +0000

    create subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "ecomm-vpc" {
     Name = "ecomm"
   }
 }
+
+# Subnet
+resource "aws_subnet" "ecomm-subnet" {
+  vpc_id     = aws_vpc.ecomm-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "ecomm-subnet"
+  }
+}
