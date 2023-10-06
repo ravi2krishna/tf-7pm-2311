6b6ad539a54b39adc441b13ad2ef6ec1fc392b8b
ravi2krishna <ravi2krishna@gmail.com>
2023-10-06 14:36:49 +0000

    HA-Pub-Pvt-Infra

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,38 @@
-# EC2 Server Setup - Web App Hosting
-resource "aws_instance" "ecomm-server" {
+# EC2 Server Setup - A - AZ - Web App Hosting
+resource "aws_instance" "ecomm-server-a" {
   ami           = "ami-0430580de6244e02e"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.ecomm-subnet.id
+  subnet_id = aws_subnet.ecomm-pub-subnet-a.id
   key_name = "ravi-key"
-  vpc_security_group_ids = [aws_security_group.ecomm-sg.id]
+  vpc_security_group_ids = [aws_security_group.ecomm-pub-sg.id]
+  user_data = file("ecomm.sh")
   tags = {
-    Name = "ecomm-server"
+    Name = "ecomm-server-a"
+  }
+}
+
+# EC2 Server Setup - B - AZ - Web App Hosting
+resource "aws_instance" "ecomm-server-b" {
+  ami           = "ami-0430580de6244e02e"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.ecomm-pub-subnet-b.id
+  key_name = "ravi-key"
+  vpc_security_group_ids = [aws_security_group.ecomm-pub-sg.id]
+  user_data = file("ecomm.sh")
+  tags = {
+    Name = "ecomm-server-b"
+  }
+}
+
+
+# EC2 Server Setup - A - AZ - DB App Hosting
+resource "aws_instance" "ecomm-server-db-a" {
+  ami           = "ami-0430580de6244e02e"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.ecomm-pvt-subnet-a.id
+  key_name = "ravi-key"
+  vpc_security_group_ids = [aws_security_group.ecomm-pvt-sg.id]
+  tags = {
+    Name = "ecomm-server-db-a"
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -8,14 +8,47 @@ resource "aws_vpc" "ecomm-vpc" {
   }
 }
 
-# Subnet
-resource "aws_subnet" "ecomm-subnet" {
+# Public Subnet - AZ - A
+resource "aws_subnet" "ecomm-pub-subnet-a" {
   vpc_id     = aws_vpc.ecomm-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2a"
   map_public_ip_on_launch = "true"
   tags = {
-    Name = "ecomm-subnet"
+    Name = "ecomm-pub-subnet-A"
+  }
+}
+
+# Public Subnet - AZ - B
+resource "aws_subnet" "ecomm-pub-subnet-b" {
+  vpc_id     = aws_vpc.ecomm-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "ecomm-pub-subnet-B"
+  }
+}
+
+# Private Subnet - AZ - A
+resource "aws_subnet" "ecomm-pvt-subnet-a" {
+  vpc_id     = aws_vpc.ecomm-vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "ecomm-pvt-subnet-A"
+  }
+}
+
+# Private Subnet - AZ - B
+resource "aws_subnet" "ecomm-pvt-subnet-b" {
+  vpc_id     = aws_vpc.ecomm-vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "ecomm-pvt-subnet-B"
   }
 }
 
@@ -28,8 +61,8 @@ resource "aws_internet_gateway" "ecomm-gw" {
   }
 }
 
-# Route Table
-resource "aws_route_table" "ecomm-rt" {
+# Public Route Table
+resource "aws_route_table" "ecomm-pub-rt" {
   vpc_id = aws_vpc.ecomm-vpc.id
 
   route {
@@ -38,18 +71,45 @@ resource "aws_route_table" "ecomm-rt" {
   }
 
   tags = {
-    Name = "ecomm-route-table"
+    Name = "ecomm-public-route-table"
   }
 }
 
-# Subnet Association - RT
-resource "aws_route_table_association" "ecomm-asc" {
-  subnet_id      = aws_subnet.ecomm-subnet.id
-  route_table_id = aws_route_table.ecomm-rt.id
+# Private Route Table
+resource "aws_route_table" "ecomm-pvt-rt" {
+  vpc_id = aws_vpc.ecomm-vpc.id
+
+  tags = {
+    Name = "ecomm-private-route-table"
+  }
 }
 
-# Network Access Control List - NACL
-resource "aws_network_acl" "ecomm-nacl" {
+# Pub Subnet A Association - RT
+resource "aws_route_table_association" "ecomm-pub-asc-a" {
+  subnet_id      = aws_subnet.ecomm-pub-subnet-a.id
+  route_table_id = aws_route_table.ecomm-pub-rt.id
+}
+
+# Pub Subnet B Association - RT
+resource "aws_route_table_association" "ecomm-pub-asc-b" {
+  subnet_id      = aws_subnet.ecomm-pub-subnet-b.id
+  route_table_id = aws_route_table.ecomm-pub-rt.id
+}
+
+# Pvt Subnet A Association - RT
+resource "aws_route_table_association" "ecomm-pvt-asc-a" {
+  subnet_id      = aws_subnet.ecomm-pvt-subnet-a.id
+  route_table_id = aws_route_table.ecomm-pvt-rt.id
+}
+
+# Pvt Subnet B Association - RT
+resource "aws_route_table_association" "ecomm-pvt-asc-b" {
+  subnet_id      = aws_subnet.ecomm-pvt-subnet-b.id
+  route_table_id = aws_route_table.ecomm-pvt-rt.id
+}
+
+# Public Network Access Control List - NACL
+resource "aws_network_acl" "ecomm-pub-nacl" {
   vpc_id = aws_vpc.ecomm-vpc.id
 
   egress {
@@ -71,19 +131,65 @@ resource "aws_network_acl" "ecomm-nacl" {
   }
 
   tags = {
-    Name = "ecomm-nacl"
+    Name = "ecomm-public-nacl"
   }
 }
 
-# NACL - Subnet Association
-resource "aws_network_acl_association" "nacl-asc" {
-  network_acl_id = aws_network_acl.ecomm-nacl.id
-  subnet_id      = aws_subnet.ecomm-subnet.id
+
+# Private Network Access Control List - NACL
+resource "aws_network_acl" "ecomm-pvt-nacl" {
+  vpc_id = aws_vpc.ecomm-vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "ecomm-private-nacl"
+  }
 }
 
-# Security Group
-resource "aws_security_group" "ecomm-sg" {
-  name        = "ecomm-sg"
+# Public - NACL - Subnet - A - Association
+resource "aws_network_acl_association" "pub-nacl-asc-a" {
+  network_acl_id = aws_network_acl.ecomm-pub-nacl.id
+  subnet_id      = aws_subnet.ecomm-pub-subnet-a.id
+}
+
+# Public - NACL - Subnet - B - Association
+resource "aws_network_acl_association" "pub-nacl-asc-b" {
+  network_acl_id = aws_network_acl.ecomm-pub-nacl.id
+  subnet_id      = aws_subnet.ecomm-pub-subnet-b.id
+}
+
+# Private - NACL - Subnet - A - Association
+resource "aws_network_acl_association" "pvt-nacl-asc-a" {
+  network_acl_id = aws_network_acl.ecomm-pvt-nacl.id
+  subnet_id      = aws_subnet.ecomm-pvt-subnet-a.id
+}
+
+# Private - NACL - Subnet - B - Association
+resource "aws_network_acl_association" "pvt-nacl-asc-b" {
+  network_acl_id = aws_network_acl.ecomm-pvt-nacl.id
+  subnet_id      = aws_subnet.ecomm-pvt-subnet-b.id
+}
+
+# Public Security Group
+resource "aws_security_group" "ecomm-pub-sg" {
+  name        = "pub-ecomm-sg"
   description = "Allow ecomm inbound traffic"
   vpc_id      = aws_vpc.ecomm-vpc.id
 
@@ -111,6 +217,40 @@ resource "aws_security_group" "ecomm-sg" {
   }
 
   tags = {
-    Name = "ecomm-security-group"
+    Name = "ecomm-pub-security-group"
+  }
+}
+
+# Private Security Group
+resource "aws_security_group" "ecomm-pvt-sg" {
+  name        = "pvt-ecomm-sg"
+  description = "Allow ecomm inbound traffic"
+  vpc_id      = aws_vpc.ecomm-vpc.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    description      = "MySQL"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecomm-pvt-security-group"
   }
 }
