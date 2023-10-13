0a3118d995c5f6bab888cce66a3a3a29a28fa3df
ravi2krishna <ravi2krishna@gmail.com>
2023-10-13 13:57:23 +0000

    database nsg

--- vnet.tf
+++ vnet.tf
@@ -59,3 +59,38 @@ resource "azurerm_network_security_group" "ecomm-web-nsg" {
     env = "prod"
   }
 }
+
+# Database Network Security Group
+resource "azurerm_network_security_group" "ecomm-db-nsg" {
+  name                = "ecomm-database-firewall"
+  location            = azurerm_resource_group.ecomm-rg.location
+  resource_group_name = azurerm_resource_group.ecomm-rg.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.0.0/16"
+  }
+
+    security_rule {
+    name                       = "mysql"
+    priority                   = 1100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.0.0/16"
+  }
+
+  tags = {
+    env = "prod"
+  }
+}
