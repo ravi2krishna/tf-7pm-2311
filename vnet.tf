6752429ec949adda35a63594d10a7f8fdb18fd75
ravi2krishna <ravi2krishna@gmail.com>
2023-10-13 14:19:01 +0000

    web nic

--- vnet.tf
+++ vnet.tf
@@ -118,3 +118,17 @@ resource "azurerm_public_ip" "ecomm-web-pip" {
     env = "prod"
   }
 }
+
+# Ecomm Web NIC
+resource "azurerm_network_interface" "ecomm-web-nic" {
+  name                = "ecomm-web-nic"
+  location            = azurerm_resource_group.ecomm-rg.location
+  resource_group_name = azurerm_resource_group.ecomm-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.ecomm-web-sn.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.ecomm-web-pip.id
+  }
+}
